83f27912381b42bf89b66edc05187763de7b81a0
Ruan Silva <progmruansilva@gmail.com>
2025-06-18 22:54:39 -0300

    🚀 Change css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <link href="/src/lib.css" rel="stylesheet" />
+    <link href="/src/App.css" rel="stylesheet" />
     <App />
   </StrictMode>,
-)
+);
